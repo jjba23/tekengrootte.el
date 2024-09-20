00c10fd5f82c09d04de0129703c11986e21035b4
Josep Bigorra <jjbigorra@gmail.com>
2024-09-20 18:36:46 +0200

    wip

---
+++ tekengrootte.el
@@ -0,0 +1,34 @@
+;;; tekengrootte.el --- proportionally sized faces -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Free Software Foundation, Inc.
+
+;; Version: 0.1.0
+;; Author: Josep Bigorra <jjbigorra@gmail.com>
+;; Maintainer: Josep Bigorra <jjbigorra@gmail.com>
+;; Keywords: font, face, size, proportional
+;; Package: emacs
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+
+(provide 'tekengrootte)
+
+;;; tekengrootte.el ends here
